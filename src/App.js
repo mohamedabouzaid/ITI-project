7a31bb9577996a2ee4c462b27e4c334fa39e097f
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-23 23:09:02 +0200

    login

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import MainNavigation from './shared/components/Navigation/MainNavigation'
 import Login from './pages/Registration/Login'
 import Signup from './pages/Registration/Signup'
 import ErrorPage from './pages/handler/ErrorPage'
-<<<<<<< Updated upstream
 import Product from './pages/Product/Product'
 import Categories from './pages/Categories/categories'
-
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
->>>>>>> Stashed changes
+
+
 function App() {
   return (
     
