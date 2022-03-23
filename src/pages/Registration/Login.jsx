12b0f7762e84a05212387e5bca150b587fff1402
Mustafa Rostom <mutafarostom@gmail.com>
2022-03-23 21:20:24 +0200

    add sign up and login

--- src/pages/Registration/Login.jsx
+++ src/pages/Registration/Login.jsx
@@ -1,12 +1,86 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+  const validate = values => {
+		const errors = {};
+		if (!values.email) {
+			errors.email = 'Required';
+		} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+			errors.email = 'Invalid email address';
+		}
+
+		if (!values.password) {
+			errors.password = 'Required';
+		} else if (values.password.length < 8) {
+			errors.password = 'Must be 8 characters or more';
+		}
+		return errors;
+	};
+	const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <>
+      <Form onSubmit={formik.handleSubmit} className="w-50 m-auto">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <h1>Login</h1>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            className={
+              formik.touched.email && formik.errors.email ? "is-invalid" : ""
+            }
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            autoComplete="on"
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-danger">{formik.errors.email}</div>
+          ) : null}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            className={
+              formik.touched.password && formik.errors.password
+                ? "is-invalid"
+                : ""
+            }
+            type="password"
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            placeholder="Password"
+            autoComplete="on"
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="text-danger">{formik.errors.password}</div>
+          ) : null}
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Log In
+        </Button>
+      </Form>
+    </>
+  );
 };
 
-
 export default Login;
+
--- src/pages/Registration/Signup.jsx
+++ src/pages/Registration/Signup.jsx
@@ -1,11 +1,143 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Signup = () => {
-    return (
-        <div>
-            signUp
-        </div>
-    );
+  const validate = (values) => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Required";
+    } else if (values.name.length < 3) {
+      errors.name = "Must be 3 characters or more";
+    }
+
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password.length < 8) {
+      errors.password = "Must be 8 characters or more";
+    }
+
+    if (!values.passwordConfirmation) {
+      errors.passwordConfirmation = "Required";
+    } else if (values.passwordConfirmation !== values.password) {
+      errors.passwordConfirmation = "passwords don't match";
+    }
+
+    return errors;
+  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  });
+  return (
+      
+    <Form onSubmit={formik.handleSubmit} className="w-50 m-auto">
+      <Form.Group className="mb-3" controlId="formBasicText">
+          <h1>Sign Up</h1>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          className={
+            formik.touched.name && formik.errors.name ? "is-invalid" : ""
+          }
+          required
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+          autoComplete="on"
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div className="text-danger">{formik.errors.name}</div>
+        ) : null}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          className={
+            formik.touched.email && formik.errors.email ? "is-invalid" : ""
+          }
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          autoComplete="on"
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="text-danger">{formik.errors.email}</div>
+        ) : null}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          className={
+            formik.touched.password && formik.errors.password
+              ? "is-invalid"
+              : ""
+          }
+          type="password"
+          name="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+          placeholder="Password"
+          autoComplete="on"
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div className="text-danger">{formik.errors.password}</div>
+        ) : null}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPasswordConfirmation">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control
+          className={
+            formik.touched.passwordConfirmation &&
+            formik.errors.passwordConfirmation
+              ? "is-invalid"
+              : ""
+          }
+          type="password"
+          name="passwordConfirmation"
+          placeholder="Password Confirmation"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.passwordConfirmation}
+          autoComplete="on"
+        />
+        {formik.touched.passwordConfirmation &&
+        formik.errors.passwordConfirmation ? (
+          <div className="text-danger">
+            {formik.errors.passwordConfirmation}
+          </div>
+        ) : null}
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign Up
+      </Button>
+    </Form>
+    
+  );
 };
 
 export default Signup;
