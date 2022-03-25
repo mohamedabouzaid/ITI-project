98fdddb3fc03a8c9312e35d248813e25ad1b9887
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-25 13:27:58 +0200

    fix issue in paggation

--- src/shared/components/UI/paggation.jsx
+++ src/shared/components/UI/paggation.jsx
@@ -7,11 +7,20 @@ const data={
 }
 const Arr =[1,2,3,4,5,6]
 const Paggation = (props) => {
-  
+ 
    const {filter,catName}=props
-   const pre=filter-1;
-   const nex=+filter+1;
-   console.log(pre,nex);
+   let pre=filter-1;
+    let nex=+filter+1;
+  
+    if(+filter===1){
+      pre=1
+    }
+ else if(filter==Arr.length){  
+   nex=Arr.length;
+ }
+
+   console.log(filter);
+  
    const navLink=`/category/${catName}/`
     return (
         <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
@@ -38,6 +47,7 @@ const Paggation = (props) => {
           </div>
           <div>
             <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+         
               <Link
                 to={navLink+pre}
                 className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
@@ -45,6 +55,7 @@ const Paggation = (props) => {
                 <span className="sr-only">Previous</span>
                 <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
               </Link>
+          
               {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
               
              {
@@ -73,7 +84,7 @@ const Paggation = (props) => {
              
              
 
-
+            
               <Link
                 to={navLink+nex}
                 className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
@@ -81,6 +92,7 @@ const Paggation = (props) => {
                 <span className="sr-only">Next</span>
                 <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
               </Link>
+            
             </nav>
           </div>
         </div>
